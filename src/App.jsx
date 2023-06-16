016b8aa5e318c1ef3e840801e12100b95de0675d
Way2SelfAware <neo@DESKTOP-DDPIL1P>
2023-06-15 19:30:02 -0700

    stopped on step 7 of the workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // Write your Color component here
-
+const Color = (props) => {
+  return <div className="red"></div>
+}
 const App = () => {
   return (
     <div id="container">
@@ -7,7 +9,11 @@ const App = () => {
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color />
+        <Color />
+        <Color />
+      </div>
     </div>
   );
 };
